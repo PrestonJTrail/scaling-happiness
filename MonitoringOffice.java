559d96367b32959f7e7b6c7d3d0ba245fee00504
KevinVasovic <KevinVasovic@users.noreply.github.com>
2017-04-07 15:09:03 +1000

    Revert "Update MonitoringOffice.java"

--- MonitoringOffice.java
+++ MonitoringOffice.java
@@ -281,21 +281,7 @@ public class MonitoringOffice extends JFrame implements ActionListener, KeyListe
     
     public static void InsertionSortVelocity(ArrayList<Object[]> arr, JTable myTable)
     {
-        {
-	    int j; // the number of items sorted so far
-	    int key; // the item to be inserted
-	    int i;
-	    
-	    for (j = 1; j < num.length; j++) // Start with 1 (not 0)
-	    {
-		key = num[ j ];
-		for(i = j - 1; (i >= 0) && (num[ i ] < key); i--) // Smaller values are moving up
-	    {
-		num[ i+1 ] = num[ i ];
-	    }
-		num[ i+1 ] = key; // Put the key in its proper location
-	    }
-	}
+        //TODO
     }
 //</editor-fold>
     
